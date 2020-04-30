25a61d66315a3002b59e5e9dbf8ab7139082df6b
s.gilbert <simon.gilbert@animal-dynamics.com>
2020-04-30 01:21:46 +0100

    Added macros for calculating button counts for various task frequencies. updated comments. Pushed static tasks to bottom of source file.

--- APL/Inc/ButtonHandler.hpp
+++ APL/Inc/ButtonHandler.hpp
@@ -23,16 +23,39 @@ INCLUDES
 #include "LinkedList.hpp"
 #include "DebounceTask.hpp"
 #include "PneumaticActuator.hpp"
+#include "stdint.h"
 
 /*******************************************************************************
 DEFINITIONS
 *******************************************************************************/
 
-typedef void (* GPIOcallbackFuncPtr_t)(void *);
+typedef void (* GPIOcallbackFuncPtr_t)(void *);//(void *, uint32_t);
+#define BUTTONTASK_FREQUENCY_HZ (1000u)
+#define BUTTONTASK_PERIOD_MS    ((float)(1.0f / BUTTONTASK_FREQUENCY_HZ))
+
+#define SHORT_PRESS_MS          (100.0f)
+#define LONG_PRESS_MS           (2000.0f)
+
+#define SHORT_PRESS_FREQ_HZ     ((float)(1.0f / (SHORT_PRESS_MS / 1000)))
+#define LONG_PRESS_FREQ_HZ      ((float)(1.0f / (LONG_PRESS_MS / 1000)))
+
+#define SHORT_PRESS_COUNT       (BUTTONTASK_FREQUENCY_HZ * SHORT_PRESS_FREQ_HZ)
+#define LONG_PRESS_COUNT        (BUTTONTASK_FREQUENCY_HZ * LONG_PRESS_FREQ_HZ)
+
+#define MAX_BUTTONS (16)
 
 /*******************************************************************************
 TYPES
 *******************************************************************************/
+
+typedef struct
+{
+    GPIO_TypeDef * GPIO_port;
+    uint16_t GPIO_pin;
+    uint32_t pull_up_down;
+    callbackFuncPtr_t assertCallback;
+    callbackFuncPtr_t deassertCallback;
+}buttonData_t;
 
 /*******************************************************************************
 GLOBAL VARIABLES
@@ -66,20 +89,20 @@ public:
     ~CbuttonHandler(void) = default;
     void funcBegin(void) override;
     void funcMain(void) override;
-
-private:
     void subscribe(GPIO_TypeDef * GPIO_port
                    , uint16_t GPIO_pin
                    , uint32_t pull_up_down
-                   , GPIOcallbackFuncPtr_t shortPressCallback
-                   , GPIOcallbackFuncPtr_t longPressCallback
-                   , void * pInstance);
-    static void _press(void * pInstance);
-    static void _release(void * pInstance);
-    static void _shortButtonPress(void * pInstance);
-    static void _longButtonPress(void * pInstance);
+                   , GPIOcallbackFuncPtr_t shortPressCallback = nullptr
+                   , GPIOcallbackFuncPtr_t longPressCallback = nullptr);
+    void subscribe(buttonData_t * pButtonData);
+
+private:
     void pressed();
     void released();
+    static void _press(void * pInstance);
+    static void _release(void * pInstance);
+//    static void _shortButtonPress(void * pInstance, uint32_t button);
+//    static void _longButtonPress(void * pInstance, uint32_t button);
 
 private:
     uint32_t m_changed;
@@ -89,7 +112,7 @@ private:
     CLinkedList<CGPIOCallback> m_buttonCounter;
     CDebounceTask * m_pDebounceTask;
     CPneumaticActuator * m_pActuator;
-
+    uint8_t GPIOTable[MAX_BUTTONS * sizeof(CNode<CbuttonHandler::CGPIOCallback>)];
 };
 
 /*******************************************************************************
--- APL/Src/ButtonHandler.cpp
+++ APL/Src/ButtonHandler.cpp
@@ -16,7 +16,6 @@ INCLUDES
 *******************************************************************************/
 
 #include "ButtonHandler.hpp"
-#include "main.h"
 
 /*******************************************************************************
 NAMESPACE
@@ -25,10 +24,6 @@ NAMESPACE
 /*******************************************************************************
 DEFINITIONS
 *******************************************************************************/
-
-#define SHORT_PRESS (10)
-#define LONG_PRESS  (100)
-#define MAX_GPIOS (16)
 
 /*******************************************************************************
 TYPES
@@ -41,8 +36,6 @@ GLOBAL VARIABLES
 /*******************************************************************************
 MODULE VARIABLES
 *******************************************************************************/
-
-uint8_t GPIOTable[MAX_GPIOS * sizeof(CNode<CbuttonHandler::CGPIOCallback>)];
 
 /*******************************************************************************
 MODULE FUNCTION DECLARATIONS
@@ -79,37 +72,39 @@ CbuttonHandler::CbuttonHandler(char const * const pName
     , m_buttonCounter(GPIOTable, sizeof(GPIOTable))
     , m_pDebounceTask(pDebounceTask)
     , m_pActuator(pActuator)
-{
+{}
 
-}
-
+/**\brief   Constructor.
+ *
+ * \param   pName       - pointer to the task name
+ * \param   freq        - task update frequency
+ * \param   priority    - task priority
+ * \param   pStack      - pointer to stack space
+ * \param   stackSize   - stack size in bytes
+ * \param   pWatchdog   - pointer to the watchdog hardware
+ *
+ * \return  None
+ */
 void CbuttonHandler::funcBegin(void)
 {
-    this->subscribe(Button1_GPIO_Port, Button1_Pin, GPIO_PULLUP
-                    , _shortButtonPress
-                    , _longButtonPress
-                    , this);
+//    this->subscribe(Button1_GPIO_Port, Button1_Pin, GPIO_PULLUP
+//                    , _shortButtonPress
+//                    , _longButtonPress);
 //    this->subscribe(Button2_GPIO_Port, Button2_Pin, GPIO_PULLUP);
 //    this->subscribe(Button3_GPIO_Port, Button3_Pin, GPIO_PULLUP);
 //    this->subscribe(Button4_GPIO_Port, Button4_Pin, GPIO_PULLUP);
 }
 
-void CbuttonHandler::_shortButtonPress(void * pInstance)
-{
-
-}
-
-
-void CbuttonHandler::_longButtonPress(void * pInstance)
-{
-
-}
-
-/**\brief   Registers a callback to the specified port and pin combination.
+/**\brief   Registers a pin and port combination to be monitored by the button
+ *          handler task. Independent callbacks can be registered for both short
+ *          and long presses.
+ *
  *
  * \param   GPIO_port           - pointer to the port struct
  * \param   GPIO_pin            - pin ID
  * \param   pull_up_down        - one of GPIO_NOPULL, GPIO_PULLUP or GPIO_PULLDOWN
+ * \param   shortPressCallback  - short press callback function pointer
+ * \param   longPressCallback   - long press callback function pointer
  *
  * \return  returns true if successful otherwise returns false
  */
@@ -117,12 +112,11 @@ void CbuttonHandler::subscribe(GPIO_TypeDef * GPIO_port
                                , uint16_t GPIO_pin
                                , uint32_t pull_up_down
                                , GPIOcallbackFuncPtr_t shortPressCallback
-                               , GPIOcallbackFuncPtr_t longPressCallback
-                               , void * pInstance)
+                               , GPIOcallbackFuncPtr_t longPressCallback)
 {
     m_pDebounceTask->subscribe(GPIO_port, GPIO_pin, pull_up_down
-                  , _release, this
-                  , _press, this);
+                              , _release, this
+                              , _press, this);
 
     CGPIOCallback GPIOState = {0
                                 , shortPressCallback
@@ -132,28 +126,38 @@ void CbuttonHandler::subscribe(GPIO_TypeDef * GPIO_port
     m_buttonCounter.pushToFront(&GPIOState);
 }
 
-void CbuttonHandler::_press(void * pInstance)
+/**\brief   Registers a pin and port combination to be monitored by the button
+ *          handler task. Independent callbacks can be registered for both short
+ *          and long presses.
+ *
+ * \param   pButtonData - pointer to a struct containing the button data.
+ *
+ * \return  returns true if successful otherwise returns false
+ */
+void CbuttonHandler::subscribe(buttonData_t * pButtonData)
 {
-    if(nullptr != pInstance)
-    {
-        ((CbuttonHandler *)pInstance)->pressed();
-    }
+    this->subscribe(pButtonData->GPIO_port, pButtonData->GPIO_pin, pButtonData->pull_up_down
+                    , pButtonData->assertCallback, pButtonData->deassertCallback);
 }
 
-void CbuttonHandler::_release(void * pInstance)
-{
-    if(nullptr != pInstance)
-    {
-        ((CbuttonHandler *)pInstance)->released();
-    }
-}
-
-void CbuttonHandler::pressed()
+/**\brief   Sets the bit that indicates which button has been pressed.
+ *
+ * \param   None
+ *
+ * \return  None
+ */
+void CbuttonHandler::pressed(void)
 {
     m_buttonState |= m_pDebounceTask->anyChangedState();
 }
 
-void CbuttonHandler::released()
+/**\brief   Clears the bit that indicates which button has been released.
+ *
+ * \param   None
+ *
+ * \return  None
+ */
+void CbuttonHandler::released(void)
 {
     m_buttonState &= ~m_pDebounceTask->anyChangedState();
 }
@@ -167,10 +171,10 @@ void CbuttonHandler::released()
 void CbuttonHandler::funcMain(void)
 {
     auto buttonState = m_buttonState;
+    auto buttonCounter = 0u;
 
     while (buttonState)
     {
-        auto buttonCounter = 0u;
         if(buttonState && 0x01)
         {
             CGPIOCallback GPIOState;
@@ -179,12 +183,12 @@ void CbuttonHandler::funcMain(void)
             m_buttonCounter.updateNode(buttonCounter, &GPIOState);
 
             // check for a long press (and still being held)
-            if (LONG_PRESS < GPIOState.counter)
+            if (LONG_PRESS_COUNT < GPIOState.counter)
             {
                 if(GPIOState.longPressCallback)
                 {
                     GPIOState.longPressCallback(GPIOState.pInstance);
-                    GPIOState.counter = LONG_PRESS / 2; // reset the timer so it now takes half the long press period, this allows the user to quickly move through a list without going too fast
+                    GPIOState.counter = LONG_PRESS_COUNT / 2; // reset the timer so it now takes half the long press period, this allows the user to quickly move through a list without going too fast
                     m_buttonCounter.updateNode(buttonCounter, &GPIOState);
                 }
             }
@@ -193,7 +197,7 @@ void CbuttonHandler::funcMain(void)
         ++buttonCounter;
     }
 
-    uint32_t releasedButtons = (m_buttonState ^ m_previousButtonState);
+    uint32_t releasedButtons = m_previousButtonState & (m_buttonState ^ m_previousButtonState);
     m_previousButtonState = m_buttonState;
 
     uint32_t buttonCount = 0;
@@ -204,8 +208,8 @@ void CbuttonHandler::funcMain(void)
             CGPIOCallback GPIOState;
             m_buttonCounter.peakFromNode(buttonCount, &GPIOState);
 
-            if ((SHORT_PRESS < GPIOState.counter)
-                    && (GPIOState.counter < LONG_PRESS))
+            if ((SHORT_PRESS_COUNT < GPIOState.counter)
+                    && (GPIOState.counter < SHORT_PRESS_COUNT))
             {
                 if(GPIOState.shortPressCallback)
                 {
@@ -221,3 +225,58 @@ void CbuttonHandler::funcMain(void)
     }
 }
 
+/**\brief   static function to call the class's press method.
+ *
+ * \param   pInstance   - class instance
+ *
+ * \return  None
+ */
+void CbuttonHandler::_press(void * pInstance)
+{
+    if(nullptr != pInstance)
+    {
+        ((CbuttonHandler *)pInstance)->pressed();
+    }
+}
+
+/**\brief   static function to call the class's release method.
+ *
+ * \param   pInstance   - class instance
+ *
+ * \return  None
+ */
+void CbuttonHandler::_release(void * pInstance)
+{
+    if(nullptr != pInstance)
+    {
+        ((CbuttonHandler *)pInstance)->released();
+    }
+}
+
+/**\brief   static function to call the class's short press method.
+ *
+ * \param   pInstance   - class instance
+ * \param   button      - button ID
+ *
+ * \return  None
+ */
+//void CbuttonHandler::_shortButtonPress(void * pInstance, uint32_t button)
+//{
+//    CGPIOCallback GPIOState;
+//
+//    ((CbuttonHandler *)pInstance)->m_buttonCounter.peakFromNode(button, &GPIOState);
+//
+//    GPIOState.shortPressCallback(pInstance);
+//}
+
+/**\brief   static function to call the class's long press method.
+ *
+ * \param   pInstance   - class instance
+ * \param   button      - button ID
+ *
+ * \return  None
+ */
+//void CbuttonHandler::_longButtonPress(void * pInstance, uint32_t button)
+//{
+//
+//}
